6db53289f89787fe727f3b078e583c9d2bfa3953
Keyth Roy <keythroy@gmail.com>
2023-10-26 15:31:28 -0300

    feature: adding littlelemon  api

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,21 +3,23 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, De
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
