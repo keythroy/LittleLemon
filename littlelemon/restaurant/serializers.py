173e7cfc9c53df3da991d1fcda67d759bd461abd
Keyth Roy <keythroy@gmail.com>
2023-10-27 12:24:29 -0300

    feature: adding unit testing to restaurant api

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,12 +1,14 @@
 from .models import Menu, Booking
 from rest_framework import serializers
 
+
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
-        fields = '__all__'
-        
+        fields = "__all__"
+
+
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = "__all__"
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,8 +3,8 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('menu/items/', views.MenuItemsView.as_view()),
-    path('menu/items/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('api-token-auth/', obtain_auth_token),
+    path("", views.index, name="index"),
+    path("menu/items/", views.MenuItemsView.as_view()),
+    path("menu/items/<int:pk>", views.SingleMenuItemView.as_view()),
+    path("api-token-auth/", obtain_auth_token),
 ]
